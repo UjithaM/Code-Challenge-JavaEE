da94bbcf218022c0f577d84fc8bd44651161951c
Ujitha <ujithamigara@gmail.com>
2024-01-15 19:48:22 +0530

    complete ItemBOImpl

--- src/main/java/software/ujithamigara/codechallengejavaee/bo/custom/Impl/ItemBOImpl.java
+++ src/main/java/software/ujithamigara/codechallengejavaee/bo/custom/Impl/ItemBOImpl.java
@@ -2,28 +2,38 @@ package software.ujithamigara.codechallengejavaee.bo.custom.Impl;
 
 import software.ujithamigara.codechallengejavaee.DTO.ItemDTO;
 import software.ujithamigara.codechallengejavaee.bo.custom.ItemBO;
+import software.ujithamigara.codechallengejavaee.dao.DAOFactory;
+import software.ujithamigara.codechallengejavaee.dao.custom.ItemDAO;
+import software.ujithamigara.codechallengejavaee.entity.Item;
+import software.ujithamigara.codechallengejavaee.util.Convert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemBOImpl implements ItemBO {
-
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ITEM);
     @Override
     public List<ItemDTO> getAllItems() throws Exception {
-        return null;
+        List<Item> items = itemDAO.getAll();
+        List<ItemDTO> itemDTOS = new ArrayList<>();
+        for (Item item : items) {
+            itemDTOS.add(Convert.itemEntityToDto(item));
+        }
+        return itemDTOS;
     }
 
     @Override
     public boolean saveItem(ItemDTO dto) throws Exception {
-        return false;
+        return itemDAO.save(Convert.itemDTOToEntity(dto, null));
     }
 
     @Override
     public boolean updateItem(ItemDTO dto) throws Exception {
-        return false;
+        return itemDAO.update(Convert.itemDTOToEntity(dto, null));
     }
 
     @Override
     public boolean deleteItem(String id) throws Exception {
-        return false;
+        return itemDAO.delete(id);
     }
 }
