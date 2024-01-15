fe2af5e8a478df27e12219f3dfa738e49530077a
Ujitha <ujithamigara@gmail.com>
2024-01-15 12:16:06 +0530

    add Convert to handle entity and DTO convert

--- src/main/java/software/ujithamigara/codechallengejavaee/entity/Customer.java
+++ src/main/java/software/ujithamigara/codechallengejavaee/entity/Customer.java
@@ -23,4 +23,7 @@ public class Customer {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Orders> orders;
 
+    public Customer(String customerId) {
+        this.customerId = customerId;
+    }
 }
---
+++ src/main/java/software/ujithamigara/codechallengejavaee/util/Convert.java
@@ -0,0 +1,41 @@
+package software.ujithamigara.codechallengejavaee.util;
+
+
+
+import software.ujithamigara.codechallengejavaee.DTO.CustomerDTO;
+import software.ujithamigara.codechallengejavaee.DTO.ItemDTO;
+import software.ujithamigara.codechallengejavaee.DTO.OrderDTO;
+import software.ujithamigara.codechallengejavaee.entity.Customer;
+import software.ujithamigara.codechallengejavaee.entity.Item;
+import software.ujithamigara.codechallengejavaee.entity.OrderItem;
+import software.ujithamigara.codechallengejavaee.entity.Orders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Convert {
+
+    public static Customer customerDTOToEntity(CustomerDTO dto, List<Orders> orderItems){
+        return new Customer(dto.getCustomerId(), dto.getFullName(), dto.getAddress(), orderItems);
+    }
+    public static CustomerDTO customerEntityToDto(Customer customer){
+        return new CustomerDTO(customer.getCustomerId(), customer.getFullName(), customer.getAddress());
+    }
+
+
+    public static Item itemDTOToEntity(ItemDTO dto, List<OrderItem> orderItems){
+        return new Item(dto.getItemId(), dto.getDescription(), dto.getUnitPrice(), dto.getQty(), orderItems);
+    }
+    public static ItemDTO itemEntityToDto(Item item){
+        return new ItemDTO(item.getItemId(), item.getDescription(), item.getUnitPrice(), item.getQty());
+    }
+
+
+    public static Orders ordersDTOToOder(OrderDTO dto){
+        Customer customer = new Customer(dto.getCustomerId());
+        return new Orders(dto.getOrderId(), dto.getDate(), customer, dto.getNetTotal(), dto.getDiscount(), dto.getCash(), dto.getSubTotal(), dto.getOrderItems());
+    }
+    public static OrderDTO orderDetailEntityToDto(Orders orders){
+        return new OrderDTO(orders.getOrderId(), orders.getDate(), orders.getCustomer().getCustomerId(), orders.getNetTotal(), orders.getDiscount(), orders.getCash(), orders.getSubTotal(), orders.getOrderItems());
+    }
+}
